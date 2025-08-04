71dbd99a31d9bcfedc168e9141abdfeea931be4c
Om Prakash Parida <179730524+omprakashparida@users.noreply.github.com>
2025-08-04 22:45:06 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,5 @@
-import app from './server.js';
+import app from '../server.js';
 
-\
-
-app.get('/api/health', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Server is running 💪',
-    timestamp: new Date().toISOString()
-  });
-});
-
-
-export default app;
+export default function handler(req, res) {
+  return app(req, res);
+}
